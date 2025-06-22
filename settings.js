fed76cb0f6668161758e51c3c5d60747591cea87
disarudulmeth1236 <disarudulmeth98@gmail.com>
2025-06-22 23:15:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",// session id 💙
-HACKER : process.env.HACKER || "94704638406" //Addmin number 💙
+SESSION_ID: process.env.SESSION_ID || "𝐒𝐔𝐋𝐀-𝐌𝐃=k6ll1DpZ#SE3wJzOMeZ34F_9LP-eJ3qZDFVx2atrunh-0_tHgKUY",// session id 💙
+HACKER : process.env.HACKER || "94753390944" //Addmin number 💙
 };
